8e17eef833101b2337ede1c9c398b77f0319d6c3
0xCourtney <courtney@scottworks.io>
2022-02-18 18:03:18 -0500

    merge

--- contracts/StrategyManager.sol
+++ contracts/StrategyManager.sol
@@ -1,84 +1,118 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
+
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 import {IKnoxStrategy} from "./interfaces/IKnoxStrategy.sol";
 import {ITreasury} from "./FarmersTreasury.sol";
 
+interface IVerifier {
+    function verifySignature(bytes memory data, bytes memory signature)
+        external
+        returns (bool);
 
-interface IVerifier{
-    function verifySignature(bytes memory data, bytes memory signature) external returns(bool);
-    function getTreasuryAddress(bytes memory signature) external returns(address);
-
+    function getTreasuryAddress(bytes memory signature)
+        external
+        returns (address);
 }
 
-struct PositionRecord{
+struct PositionRecord {
     int32 epochId;
     uint64 positionSize;
     address treasury;
 }
 
 contract StrategyManager {
-
     address public owner;
     IVerifier public signatureVerifier;
     uint256 constant WEI_PER_UNIT = 10**9;
-    mapping(address => IKnoxStrategy) public strategies;  // mapping treausryAddress (in signature) => strategyLogicAddress
-    mapping(address => PositionRecord[]) public openedPositions; 
+    mapping(address => IKnoxStrategy) public strategies; // mapping treausryAddress (in signature) => strategyLogicAddress
+    mapping(address => PositionRecord[]) public openedPositions;
 
-    mapping(address => mapping(int32 => uint64) ) public totalPositionSize; // treasury address => epoch => totalPositionSize
-    mapping(address => mapping(int32 => uint64) ) public totalMMPayout; // treasury address => epoch => totalMMPayout
+    mapping(address => mapping(int32 => uint64)) public totalPositionSize; // treasury address => epoch => totalPositionSize
+    mapping(address => mapping(int32 => uint64)) public totalMMPayout; // treasury address => epoch => totalMMPayout
 
-    constructor(address _signatureVerifier){
+    constructor(address _signatureVerifier) {
         owner = msg.sender;
         signatureVerifier = IVerifier(_signatureVerifier);
     }
 
     function addOrUpdateStrategy(address treasury, address strategy) public {
-        require(msg.sender == owner,"strategy-manager/not-allowed");
+        require(msg.sender == owner, "strategy-manager/not-allowed");
         strategies[treasury] = IKnoxStrategy(strategy);
     }
 
-    function transferOwnership(address newOwner) public{
-        require(msg.sender == owner,"strategy-manager/not-permitted");
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == owner, "strategy-manager/not-permitted");
         owner = newOwner;
     }
 
-    function openPosition(uint premiumAmount, bytes memory strategyParameters, bytes memory strategyParametersSignature) public{
+    function openPosition(
+        uint256 premiumAmount,
+        bytes memory strategyParameters,
+        bytes memory strategyParametersSignature
+    ) public {
         /* TODO: Add era period validation */
         uint64 normalizedPremiumAmount = uint64(premiumAmount / WEI_PER_UNIT);
 
-        require(signatureVerifier.verifySignature(strategyParameters, strategyParametersSignature),"strategy-manager/not-signed-parameters");
+        require(
+            signatureVerifier.verifySignature(
+                strategyParameters,
+                strategyParametersSignature
+            ),
+            "strategy-manager/not-signed-parameters"
+        );
 
-        address treasuryAddress = signatureVerifier.getTreasuryAddress(strategyParametersSignature);
+        address treasuryAddress = signatureVerifier.getTreasuryAddress(
+            strategyParametersSignature
+        );
 
-        uint64 positionSize = IKnoxStrategy(strategies[treasuryAddress]).getPositionSize(normalizedPremiumAmount, strategyParameters);
+        uint64 positionSize = IKnoxStrategy(strategies[treasuryAddress])
+            .getPositionSize(normalizedPremiumAmount, strategyParameters);
         ITreasury treasury = ITreasury(treasuryAddress);
         int32 epochId = treasury.currentEpoch();
 
-        treasury.trustedBorrow(uint256(positionSize)*WEI_PER_UNIT);
+        treasury.trustedBorrow(uint256(positionSize) * WEI_PER_UNIT);
 
-        require(treasury.baseToken().transferFrom(msg.sender, address(this), positionSize), "strategy-manager/premium-transfer-impossible");
-        bytes memory callData = abi.encodeWithSignature("openPosition(bytes, bytes)",abi.encode(strategyParameters, normalizedPremiumAmount));
-        (bool status,) = address(strategies[treasuryAddress]).delegatecall(callData);
+        require(
+            treasury.baseToken().transferFrom(
+                msg.sender,
+                address(this),
+                positionSize
+            ),
+            "strategy-manager/premium-transfer-impossible"
+        );
+        bytes memory callData = abi.encodeWithSignature(
+            "openPosition(bytes, bytes)",
+            abi.encode(strategyParameters, normalizedPremiumAmount)
+        );
+        (bool status, ) = address(strategies[treasuryAddress]).delegatecall(
+            callData
+        );
 
-        require(status,"strategy-manager/strategy-invocation-failed");
+        require(status, "strategy-manager/strategy-invocation-failed");
 
-        openedPositions[msg.sender].push(PositionRecord(epochId, positionSize, address(treasury)));
-        totalPositionSize[treasuryAddress][epochId] = totalPositionSize[treasuryAddress][epochId] + positionSize;
-        
+        openedPositions[msg.sender].push(
+            PositionRecord(epochId, positionSize, address(treasury))
+        );
+        totalPositionSize[treasuryAddress][epochId] =
+            totalPositionSize[treasuryAddress][epochId] +
+            positionSize;
     }
 
-    function settlePosition(address treasuryAddress) public{ /*called once per strategy/treausery for era by administrator*/
+    function settlePosition(address treasuryAddress) public {
+        /*called once per strategy/treausery for era by administrator*/
 
-        require(msg.sender == owner,"strategy-manager/not-permitted");
+        require(msg.sender == owner, "strategy-manager/not-permitted");
         ITreasury treasury = ITreasury(treasuryAddress);
         int32 epochId = treasury.currentEpoch();
 
-        bytes memory callData =  abi.encodeWithSignature("closePosition()");
+        bytes memory callData = abi.encodeWithSignature("closePosition()");
 
-        (bool status, bytes memory retVal) = address(strategies[treasuryAddress]).delegatecall(callData);
-        (uint64 payout, uint64 payback) = abi.decode(retVal,(uint64,uint64));
+        (bool status, bytes memory retVal) = address(
+            strategies[treasuryAddress]
+        ).delegatecall(callData);
+        (uint64 payout, uint64 payback) = abi.decode(retVal, (uint64, uint64));
 
         IERC20 payoutToken = IERC20(treasury.baseToken());
 
@@ -86,16 +120,19 @@ contract StrategyManager {
 
         treasury.trustedRepay(uint256(payback) * WEI_PER_UNIT);
         totalMMPayout[treasuryAddress][epochId] = payout;
-
     }
 
-    function withdrawPayout(uint256 index) public{/*called independly by any MM to get payaout associated with position*/
+    function withdrawPayout(uint256 index) public {
+        /*called independly by any MM to get payaout associated with position*/
         PositionRecord memory rec = openedPositions[msg.sender][index];
 
         uint64 totalPositionSize = totalPositionSize[rec.treasury][rec.epochId];
         uint64 totalPayout = totalMMPayout[rec.treasury][rec.epochId];
 
-        uint64 payout = uint64( uint256(rec.positionSize)* uint256(totalPayout)/uint256(totalPositionSize));
+        uint64 payout = uint64(
+            (uint256(rec.positionSize) * uint256(totalPayout)) /
+                uint256(totalPositionSize)
+        );
 
         ITreasury treasury = ITreasury(rec.treasury);
         IERC20 payoutToken = IERC20(treasury.baseToken());
@@ -104,11 +141,8 @@ contract StrategyManager {
     }
 
     function startNewPeriod(address treasury) public {
-        
-        require(msg.sender == owner,"strategy-manager/not-permitted");
+        require(msg.sender == owner, "strategy-manager/not-permitted");
         ITreasury treasury = ITreasury(treasury);
         treasury.createNewEpoch();
     }
-
-
 }
--- test/FarmersTreasury.ts
+++ test/FarmersTreasury.ts
@@ -1,11 +1,11 @@
-import hre from 'hardhat';
+import hre from "hardhat";
 import { expect } from "chai";
 import * as time from "./helpers/time";
 
-import { FarmersTreasury } from "./../typechain-types/FarmersTreasury";
-import { TestERC20 } from "./../typechain-types/TestERC20";
-import { parseLogs } from './helpers/utils';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { FarmersTreasury } from "./../types/FarmersTreasury";
+import { TestERC20 } from "./../types/TestERC20";
+import { parseLogs } from "./helpers/utils";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 const { getContractFactory } = hre.ethers;
 const { parseUnits } = hre.ethers.utils;
@@ -14,20 +14,28 @@ describe.only("Farmers Treasury", () => {
   let signer: SignerWithAddress;
   let signer2: SignerWithAddress;
   let treasury: FarmersTreasury;
-  let depositedToken : TestERC20;
-  let initSnapshotId : string;
-
+  let depositedToken: TestERC20;
+  let initSnapshotId: string;
 
   before(async function () {
     await hre.ethers.provider.send("evm_mine", []);
-    let block = await hre.ethers.provider.getBlock(await hre.ethers.provider.getBlockNumber());
+    let block = await hre.ethers.provider.getBlock(
+      await hre.ethers.provider.getBlockNumber()
+    );
     [signer, signer2] = await hre.ethers.getSigners();
     let factoryERC20 = await getContractFactory("TestERC20");
     let factoryFarmersTreasury = await getContractFactory("FarmersTreasury");
-    depositedToken = await factoryERC20.deploy("TestToken", "TT", parseUnits('1', 'ether')) as TestERC20;
-    await depositedToken.transfer(signer2.address, parseUnits('500', 'finney'));
-    treasury = (await factoryFarmersTreasury.deploy(depositedToken.address, block.timestamp, signer2.address)) as FarmersTreasury;
-
+    depositedToken = (await factoryERC20.deploy(
+      "TestToken",
+      "TT",
+      parseUnits("1", "ether")
+    )) as TestERC20;
+    await depositedToken.transfer(signer2.address, parseUnits("500", "finney"));
+    treasury = (await factoryFarmersTreasury.deploy(
+      depositedToken.address,
+      block.timestamp,
+      signer2.address
+    )) as FarmersTreasury;
   });
 
   describe("initial state", async () => {
@@ -37,45 +45,52 @@ describe.only("Farmers Treasury", () => {
     });
   });
 
-
   describe("withdraw", () => {
-
     describe("in the same epoch", () => {
-
       beforeEach(async () => {
         initSnapshotId = await hre.ethers.provider.send("evm_snapshot", []);
-        await depositedToken.approve(treasury.address, parseUnits('2000', 'gwei'));
-        await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("2000", "gwei")
+        );
+        await treasury.deposit(parseUnits("1000", "gwei"));
         await treasury.connect(signer2).createNewEpoch();
         let start = await treasury.currentEpochStart();
         await time.increaseTo(start.add(1));
-        await treasury.connect(signer2).trustedBorrow(parseUnits('1000', 'gwei'));
-        await treasury.deposit(parseUnits('900', 'gwei'));
-
+        await treasury
+          .connect(signer2)
+          .trustedBorrow(parseUnits("1000", "gwei"));
+        await treasury.deposit(parseUnits("900", "gwei"));
       });
 
       it("should immediately withdraw sum equal this epoch deposit", async () => {
         let balanceBefore = await depositedToken.balanceOf(signer.address);
-        let tx = await treasury.withdraw(parseUnits('900', 'gwei'));
+        let tx = await treasury.withdraw(parseUnits("900", "gwei"));
         await tx.wait();
         let balanceAfter = await depositedToken.balanceOf(signer.address);
-        expect(balanceBefore.add(parseUnits('900', 'gwei')).toString()).to.be.equal(balanceAfter.toString());
+        expect(
+          balanceBefore.add(parseUnits("900", "gwei")).toString()
+        ).to.be.equal(balanceAfter.toString());
       });
 
       it("should immediately withdraw sum smaller than this epoch deposit", async () => {
         let balanceBefore = await depositedToken.balanceOf(signer.address);
-        let tx = await treasury.withdraw(parseUnits('800', 'gwei'));
+        let tx = await treasury.withdraw(parseUnits("800", "gwei"));
         await tx.wait();
         let balanceAfter = await depositedToken.balanceOf(signer.address);
-        expect(balanceBefore.add(parseUnits('800', 'gwei')).toString()).to.be.equal(balanceAfter.toString());
+        expect(
+          balanceBefore.add(parseUnits("800", "gwei")).toString()
+        ).to.be.equal(balanceAfter.toString());
       });
 
       it("should only withdraw waiting amount and schedule rest if withdraw is bigger than epoch deposit", async () => {
         let balanceBefore = await depositedToken.balanceOf(signer.address);
-        let tx = await treasury.withdraw(parseUnits('1000', 'gwei'));
+        let tx = await treasury.withdraw(parseUnits("1000", "gwei"));
         await tx.wait();
         let balanceAfter = await depositedToken.balanceOf(signer.address);
-        expect(balanceBefore.add(parseUnits('900', 'gwei')).toString()).to.be.equal(balanceAfter.toString());
+        expect(
+          balanceBefore.add(parseUnits("900", "gwei")).toString()
+        ).to.be.equal(balanceAfter.toString());
         let data = await treasury.farmers(signer.address);
         expect(data.waitingAmount.toNumber()).to.be.equal(-100);
       });
@@ -86,31 +101,38 @@ describe.only("Farmers Treasury", () => {
     });
 
     describe("in next epoch", () => {
-
       beforeEach(async () => {
         initSnapshotId = await hre.ethers.provider.send("evm_snapshot", []);
-        await depositedToken.approve(treasury.address, parseUnits('2000', 'gwei'));
-        await depositedToken.connect(signer2).approve(treasury.address, parseUnits('5000', 'gwei'));
-        await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("2000", "gwei")
+        );
+        await depositedToken
+          .connect(signer2)
+          .approve(treasury.address, parseUnits("5000", "gwei"));
+        await treasury.deposit(parseUnits("1000", "gwei"));
         //console.log("DEP 1000");
         await treasury.connect(signer2).createNewEpoch();
         let start = await treasury.currentEpochStart();
         await time.increaseTo(start.add(1));
-        await treasury.connect(signer2).trustedBorrow(parseUnits('1000', 'gwei'));
-        await treasury.deposit(parseUnits('900', 'gwei'));
+        await treasury
+          .connect(signer2)
+          .trustedBorrow(parseUnits("1000", "gwei"));
+        await treasury.deposit(parseUnits("900", "gwei"));
         //console.log("DEP 900");
-        await treasury.connect(signer2).trustedRepay(parseUnits('1200', 'gwei'));
+        await treasury
+          .connect(signer2)
+          .trustedRepay(parseUnits("1200", "gwei"));
         await treasury.connect(signer2).createNewEpoch();
-
       });
 
       afterEach(async () => {
         await hre.ethers.provider.send("evm_revert", [initSnapshotId]);
       });
 
-      it("should schedule withdrawal without payout", async() => {
+      it("should schedule withdrawal without payout", async () => {
         let balanceBefore = await depositedToken.balanceOf(signer.address);
-        let tx = await treasury.withdraw(parseUnits('900', 'gwei'));
+        let tx = await treasury.withdraw(parseUnits("900", "gwei"));
         await tx.wait();
         let balanceAfter = await depositedToken.balanceOf(signer.address);
         expect(balanceBefore.toString()).to.be.equal(balanceAfter.toString());
@@ -118,22 +140,22 @@ describe.only("Farmers Treasury", () => {
         expect(farmerDataAfter.waitingAmount).to.be.equal(-900);
       });
 
-      it("should withdraw after re-request in new epoch", async() => {
+      it("should withdraw after re-request in new epoch", async () => {
         let balanceBefore = await depositedToken.balanceOf(signer.address);
-        let tx = await treasury.withdraw(parseUnits('900', 'gwei'));
+        let tx = await treasury.withdraw(parseUnits("900", "gwei"));
         await tx.wait();
         await treasury.connect(signer2).createNewEpoch();
         await treasury.withdraw(0);
         let balanceAfter = await depositedToken.balanceOf(signer.address);
-        expect(balanceBefore.add(parseUnits('900', 'gwei')).toString()).to.be.equal(balanceAfter.toString());
+        expect(
+          balanceBefore.add(parseUnits("900", "gwei")).toString()
+        ).to.be.equal(balanceAfter.toString());
         let farmerDataAfter = await treasury.farmers(signer.address);
         expect(farmerDataAfter.waitingAmount).to.be.equal(0);
-
       });
     });
 
     describe("between epochs", () => {
-
       beforeEach(async () => {
         initSnapshotId = await hre.ethers.provider.send("evm_snapshot", []);
       });
@@ -142,11 +164,9 @@ describe.only("Farmers Treasury", () => {
         await hre.ethers.provider.send("evm_revert", [initSnapshotId]);
       });
     });
-
   });
 
   describe("deposit", () => {
-
     beforeEach(async () => {
       initSnapshotId = await hre.ethers.provider.send("evm_snapshot", []);
     });
@@ -157,57 +177,87 @@ describe.only("Farmers Treasury", () => {
 
     it("should fail if called without approval", async function () {
       let tx = treasury.deposit(1000);
-      await expect(tx).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+      await expect(tx).to.be.revertedWith(
+        "ERC20: transfer amount exceeds allowance"
+      );
     });
 
     it("should succed if approval is set", async function () {
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      await treasury.deposit(parseUnits("1000", "gwei"));
     });
 
     it("should change farmer waiting deposit", async () => {
       let farmersDataBefore = await treasury.farmers(signer.address);
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      let tx = await treasury.deposit(parseUnits("1000", "gwei"));
       await tx.wait();
       let farmersDataAfter = await treasury.farmers(signer.address);
-      expect(farmersDataAfter.waitingAmount.toNumber()).to.be.equal(farmersDataBefore.waitingAmount.add(1000).toNumber());
+      expect(farmersDataAfter.waitingAmount.toNumber()).to.be.equal(
+        farmersDataBefore.waitingAmount.add(1000).toNumber()
+      );
     });
 
     it("should change total waiting amount by same value", async () => {
       let totalWaitingBefore = await treasury.totalWaitingAmount();
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      let tx = await treasury.deposit(parseUnits("1000", "gwei"));
       await tx.wait();
       let totalWaitingAfter = await treasury.totalWaitingAmount();
-      expect(totalWaitingAfter.toNumber()).to.be.equal(totalWaitingBefore.add(1000).toNumber());
+      expect(totalWaitingAfter.toNumber()).to.be.equal(
+        totalWaitingBefore.add(1000).toNumber()
+      );
     });
 
     it("should not change total available amount", async () => {
       let totalWaitingBefore = await treasury.totalAvailableAmount();
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      let tx = await treasury.deposit(parseUnits("1000", "gwei"));
       await tx.wait();
       let totalWaitingAfter = await treasury.totalAvailableAmount();
-      expect(totalWaitingAfter.toNumber()).to.be.equal(totalWaitingBefore.toNumber());
+      expect(totalWaitingAfter.toNumber()).to.be.equal(
+        totalWaitingBefore.toNumber()
+      );
     });
 
     it("should increase treasury token balance", async () => {
       let balanceBefore = await depositedToken.balanceOf(treasury.address);
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      let tx = await treasury.deposit(parseUnits("1000", "gwei"));
       await tx.wait();
       let balanceAfter = await depositedToken.balanceOf(treasury.address);
-      expect(balanceBefore.toNumber()).to.be.equal(balanceAfter.sub(parseUnits('1000', 'gwei')).toNumber());
+      expect(balanceBefore.toNumber()).to.be.equal(
+        balanceAfter.sub(parseUnits("1000", "gwei")).toNumber()
+      );
     });
 
     it("should decrease users token balance", async () => {
       let balanceBefore = await depositedToken.balanceOf(signer.address);
-      await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-      let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+      await depositedToken.approve(
+        treasury.address,
+        parseUnits("1000", "gwei")
+      );
+      let tx = await treasury.deposit(parseUnits("1000", "gwei"));
       await tx.wait();
       let balanceAfter = await depositedToken.balanceOf(signer.address);
-      expect(balanceAfter.toString()).to.be.equal(balanceBefore.sub(parseUnits('1000', 'gwei')).toString());
+      expect(balanceAfter.toString()).to.be.equal(
+        balanceBefore.sub(parseUnits("1000", "gwei")).toString()
+      );
     });
 
     describe("epoch -1", async () => {
@@ -220,19 +270,29 @@ describe.only("Farmers Treasury", () => {
       });
 
       it("should not sync farmer", async () => {
-        await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-        let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("1000", "gwei")
+        );
+        let tx = await treasury.deposit(parseUnits("1000", "gwei"));
         let receipt = await tx.wait();
         let events = await parseLogs("FarmersTreasury", receipt.events);
-        expect(events.filter((x) => x.name === "FarmerSynced").length).to.be.equal(0);
+        expect(
+          events.filter((x) => x.name === "FarmerSynced").length
+        ).to.be.equal(0);
       });
 
       it("should emit deposit event", async () => {
-        await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-        let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("1000", "gwei")
+        );
+        let tx = await treasury.deposit(parseUnits("1000", "gwei"));
         let receipt = await tx.wait();
         let events = await parseLogs("FarmersTreasury", receipt.events);
-        expect(events.filter((x) => x.name === "FundsDeposited").length).to.be.equal(1);
+        expect(
+          events.filter((x) => x.name === "FundsDeposited").length
+        ).to.be.equal(1);
       });
     });
 
@@ -247,23 +307,30 @@ describe.only("Farmers Treasury", () => {
       });
 
       it("should sync farmer", async () => {
-        await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-        let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("1000", "gwei")
+        );
+        let tx = await treasury.deposit(parseUnits("1000", "gwei"));
         let receipt = await tx.wait();
         let events = await parseLogs("FarmersTreasury", receipt.events);
-        expect(events.filter((x) => x.name === "FarmerSynced").length).to.be.equal(1);
+        expect(
+          events.filter((x) => x.name === "FarmerSynced").length
+        ).to.be.equal(1);
       });
 
       it("should emit deposit event", async () => {
-        await depositedToken.approve(treasury.address, parseUnits('1000', 'gwei'));
-        let tx = await treasury.deposit(parseUnits('1000', 'gwei'));
+        await depositedToken.approve(
+          treasury.address,
+          parseUnits("1000", "gwei")
+        );
+        let tx = await treasury.deposit(parseUnits("1000", "gwei"));
         let receipt = await tx.wait();
         let events = await parseLogs("FarmersTreasury", receipt.events);
-        expect(events.filter((x) => x.name === "FundsDeposited").length).to.be.equal(1);
+        expect(
+          events.filter((x) => x.name === "FundsDeposited").length
+        ).to.be.equal(1);
       });
     });
-
   });
-
-
 });
