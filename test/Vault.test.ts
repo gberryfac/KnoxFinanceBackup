c86e5f2eb2edb7b7a4940cd9ad156c5ac738244f
alfred <53237587+fuzzylemma@users.noreply.github.com>
2022-05-25 13:23:08 -0400

    borrow test coverage (#19)

--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -312,10 +312,7 @@ function behavesLikeOptionsVault(params: {
         assert.bnEqual(queuedWithdrawShares, BigNumber.from("0"));
         assert.bnEqual(queuedWithdrawals, BigNumber.from("0"));
         assert.bnNotEqual(expiry, BigNumber.from("0"));
-        assert.bnEqual(
-            await vaultContract.totalBalance(),
-            BigNumber.from("0")
-        );
+        assert.bnEqual(await vaultContract.totalBalance(), BigNumber.from("0"));
 
         // Check State Variables
         assert.equal(await vaultContract.owner(), addresses.owner);
@@ -554,9 +551,9 @@ function behavesLikeOptionsVault(params: {
       time.revertToSnapshotAfterEach();
 
       it("should revert if not owner", async () => {
-        await expect(
-          vaultContract.setStrategy(ADDRESS_ONE)
-        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(vaultContract.setStrategy(ADDRESS_ONE)).to.be.revertedWith(
+          "Ownable: caller is not the owner"
+        );
       });
 
       it("should revert when setting 0x0 as strategy", async () => {
@@ -567,9 +564,7 @@ function behavesLikeOptionsVault(params: {
 
       it("should revert when new strategy equals old strategy ", async () => {
         await expect(
-          vaultContract
-            .connect(signers.owner)
-            .setStrategy(addresses.strategy)
+          vaultContract.connect(signers.owner).setStrategy(addresses.strategy)
         ).to.be.revertedWith("11");
       });
 
@@ -583,7 +578,6 @@ function behavesLikeOptionsVault(params: {
         await vaultContract.connect(signers.owner).setStrategy(ADDRESS_ONE);
         assert.equal(await vaultContract.strategy(), ADDRESS_ONE);
       });
-
     });
 
     describe("#setFeeRecipient", () => {
@@ -644,9 +638,7 @@ function behavesLikeOptionsVault(params: {
 
       it("should revert when new keeper equals old keeper ", async () => {
         await expect(
-          vaultContract
-            .connect(signers.owner)
-            .setKeeper(addresses.keeper)
+          vaultContract.connect(signers.owner).setKeeper(addresses.keeper)
         ).to.be.revertedWith("11");
       });
 
@@ -860,13 +852,16 @@ function behavesLikeOptionsVault(params: {
 
       it("should return correct amount with no locked capital ", async () => {
         await assetContract
-            .connect(signers.user)
-            .transfer(addresses.vault, depositAmount.div(2));
-        assert.bnEqual(await vaultContract.totalBalance(), depositAmount.div(2));
+          .connect(signers.user)
+          .transfer(addresses.vault, depositAmount.div(2));
+        assert.bnEqual(
+          await vaultContract.totalBalance(),
+          depositAmount.div(2)
+        );
 
         await assetContract
-            .connect(signers.user)
-            .transfer(addresses.vault, depositAmount.div(2));
+          .connect(signers.user)
+          .transfer(addresses.vault, depositAmount.div(2));
         assert.bnEqual(await vaultContract.totalBalance(), depositAmount);
       });
 
@@ -877,13 +872,17 @@ function behavesLikeOptionsVault(params: {
 
         let lockedCapitalAmount = depositAmount.div(2);
 
-        await vaultContract.connect(signers.strategy).borrow(lockedCapitalAmount);
+        await vaultContract
+          .connect(signers.strategy)
+          .borrow(lockedCapitalAmount);
         let vaultBalance = await assetContract.balanceOf(addresses.vault);
 
         assert.bnEqual(vaultBalance, depositAmount.sub(lockedCapitalAmount));
         assert.bnEqual(await vaultContract.totalBalance(), depositAmount);
 
-        await vaultContract.connect(signers.strategy).borrow(lockedCapitalAmount);
+        await vaultContract
+          .connect(signers.strategy)
+          .borrow(lockedCapitalAmount);
         vaultBalance = await assetContract.balanceOf(addresses.vault);
 
         assert.bnEqual(vaultBalance, BigNumber.from(0));
@@ -2116,6 +2115,114 @@ function behavesLikeOptionsVault(params: {
         await expect(tx)
           .to.emit(vaultContract, "Redeem")
           .withArgs(addresses.user, redeemAmount, 1);
+      });
+    });
+
+    describe("#borrow", () => {
+      time.revertToSnapshotAfterEach(async () => {
+        // assert test suite assumption
+        assert.bnEqual(
+          await assetContract.balanceOf(addresses.vault),
+          BigNumber.from("0")
+        );
+      });
+
+      it("should revert when not called by strategy", async () => {
+        await expect(
+          vaultContract.connect(signers.user).borrow(0)
+        ).to.be.revertedWith("unauthorized");
+      });
+
+      it("should allow when called by strategy", async () => {
+        await assetContract.connect(signers.user).transfer(addresses.vault, 1);
+        await vaultContract.connect(signers.strategy).borrow(1);
+      });
+
+      it("should revert when paused", async () => {
+        await assetContract.connect(signers.user).transfer(addresses.vault, 1);
+
+        await vaultContract.connect(signers.owner).pause();
+
+        await expect(
+          vaultContract.connect(signers.strategy).borrow(1)
+        ).to.be.revertedWith("Pausable: paused");
+      });
+
+      it("should allow borrowing after unpause", async () => {
+        await assetContract.connect(signers.user).transfer(addresses.vault, 1);
+
+        await vaultContract.connect(signers.owner).pause();
+        await vaultContract.connect(signers.owner).unpause();
+        await vaultContract.connect(signers.strategy).borrow(1);
+      });
+
+      it("should revert when the amount of borrowable assets are less than requested amount", async () => {
+        await expect(
+          vaultContract.connect(signers.strategy).borrow(1)
+        ).to.be.revertedWith("5");
+
+        await assetContract.connect(signers.user).transfer(addresses.vault, 1);
+
+        await vaultContract.connect(signers.strategy).borrow(1);
+
+        await expect(
+          vaultContract.connect(signers.strategy).borrow(1)
+        ).to.be.revertedWith("5");
+      });
+
+      it("should allow when amount of borrowable assets are greater than requested amount", async () => {
+        await vaultContract.connect(signers.strategy).borrow(0);
+
+        await assetContract.connect(signers.user).transfer(addresses.vault, 10);
+
+        await vaultContract.connect(signers.strategy).borrow(1);
+        await vaultContract.connect(signers.strategy).borrow(4);
+        await vaultContract.connect(signers.strategy).borrow(5);
+      });
+
+      it("should transfer requested asset amount from vault to strategy", async () => {
+        await assetContract
+          .connect(signers.user)
+          .transfer(addresses.vault, depositAmount);
+
+        const assetInVault = await assetContract.balanceOf(addresses.vault);
+        const toBorrow = depositAmount.div(2);
+
+        await vaultContract.connect(signers.strategy).borrow(toBorrow);
+
+        assert.bnEqual(
+          await assetContract.balanceOf(addresses.vault),
+          assetInVault.sub(toBorrow)
+        );
+        assert.bnEqual(
+          await assetContract.balanceOf(addresses.strategy),
+          toBorrow
+        );
+      });
+
+      it("should increment locked collateral accounting by requested amount", async () => {
+        await assetContract
+          .connect(signers.user)
+          .transfer(addresses.vault, depositAmount);
+
+        let lockedCollateral = BigNumber.from("0");
+        let borrowSequence = [0, 1, 2, 3];
+
+        borrowSequence.forEach(async function (toBorrow) {
+          assert.bnEqual(
+            (await vaultContract.vaultState()).lockedCollateral,
+            lockedCollateral
+          );
+
+          await vaultContract.connect(signers.strategy).borrow(toBorrow);
+
+          assert.bnEqual(
+            (await vaultContract.vaultState()).lockedCollateral,
+            lockedCollateral.add(toBorrow)
+          );
+
+          lockedCollateral = lockedCollateral.add(lockedCollateral);
+        });
       });
     });
 
