d79c52b9947948cd1d25d377678e451fce25c4f1
alfred <53237587+fuzzylemma@users.noreply.github.com>
2022-05-27 10:01:04 -0400

    2 - Sync Tests (#22)
    * vault and strategy unit tests
    
    * remove redundant asset init test
    
    * test vault.sync returns the vault's asset
    
    * vault/strategy integration tests
    
    * note Sync event's rasion d'etre

--- contracts/test/MockVault.sol
+++ contracts/test/MockVault.sol
@@ -6,11 +6,15 @@ import "hardhat/console.sol";
 contract MockVault {
     address public immutable asset;
 
+    event Sync();
+
     constructor(address _asset) {
         asset = _asset;
     }
 
-    function sync(uint256) external view returns (address) {
+    function sync(uint256) external returns (address) {
+        // NOTE: A `Sync` event is ONLY emitted for TESTING.
+        emit Sync();
         return asset;
     }
 }
