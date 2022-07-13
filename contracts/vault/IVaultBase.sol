ad39572597ca62eb9340a304ed8f3a91f84f40cb
0xCourtney <courtney@scottworks.io>
2022-07-13 12:38:18 -0400

    VaultBase Interface

--- contracts/vault/IVaultBase.sol
+++ contracts/vault/IVaultBase.sol
@@ -3,6 +3,4 @@ pragma solidity ^0.8.0;
 
 import "@solidstate/contracts/token/ERC4626/IERC4626.sol";
 
-interface IVaultBase is IERC4626 {
-    function totalCollateral() external view returns (uint256);
-}
+interface IVaultBase is IERC4626 {}
