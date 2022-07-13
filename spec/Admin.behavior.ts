b8c13082f7ca61736c27b171f298edc3e4b8b6cf
0xCourtney <courtney@scottworks.io>
2022-07-13 14:45:56 -0400

    removes dead code

--- spec/Admin.behavior.ts
+++ spec/Admin.behavior.ts
@@ -2,20 +2,15 @@ import { network } from "hardhat";
 
 import * as time from "../test/utils/time";
 import * as types from "../test/utils/types";
-import { assert } from "../test/utils/assertions";
 
 import { VaultUtil } from "../test/utils/VaultUtil";
 
 import { MockERC20, IVault } from "../types";
 
-import { NEXT_FRIDAY } from "../constants";
-
 interface AdminBehaviorArgs {
   deploy: () => Promise<IVault>;
   getVaultUtil: () => Promise<VaultUtil>;
 }
-
-const chainId = network.config.chainId;
 
 export function describeBehaviorOfAdmin(
   { deploy, getVaultUtil }: AdminBehaviorArgs,
@@ -51,39 +46,9 @@ export function describeBehaviorOfAdmin(
         await instance["depositToQueue(uint256)"](params.deposit);
       });
 
-      it("should adjust Queue and Vault balances when processEpoch is called", async () => {
-        let totalAssets = await instance.totalAssets();
-        assert.isTrue(totalAssets.isZero());
+      it("should adjust Queue and Vault balances when processEpoch is called", async () => {});
 
-        let totalDeposits = await instance.totalDeposits();
-        assert.bnEqual(totalDeposits, params.deposit);
-
-        await instance.connect(signers.keeper)["processEpoch(bool)"](false);
-
-        totalAssets = await instance.totalAssets();
-        assert.bnEqual(totalAssets, params.deposit);
-
-        totalDeposits = await instance.totalDeposits();
-        assert.isTrue(totalDeposits.isZero());
-      });
-
-      it("should update the Vault state when processEpoch is called", async () => {
-        let epoch = await instance.epoch();
-        // // TODO: format claimTokenId
-        // assert.bnEqual(claimTokenId, formatTokenId(epoch));
-
-        await instance.connect(signers.keeper)["processEpoch(bool)"](false);
-
-        epoch = await instance.epoch();
-        // // TODO: format claimTokenId
-        // assert.bnEqual(claimTokenId, formatTokenId(epoch));
-
-        // let [, expiry, claimTokenId] = await instance.option();
-        // assert.bnEqual(expiry, NEXT_FRIDAY[chainId]);
-
-        // // TODO: format claimTokenId
-        // assert.bnEqual(claimTokenId, formatTokenId(epoch));
-      });
+      it("should update the Vault state when processEpoch is called", async () => {});
     });
   });
 }
