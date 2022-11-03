4607ddc01227428e864f269328c148d76f77c658
0xCourtney <courtney@scottworks.io>
2022-11-03 12:33:35 -0400

    refactors getNextFriday

--- contracts/vault/VaultInternal.sol
+++ contracts/vault/VaultInternal.sol
@@ -736,28 +736,12 @@ contract VaultInternal is ERC4626BaseInternal, IVaultEvents, OwnableInternal {
     }
 
     /**
-     * @notice returns the Friday 8AM timestamp of the following week
+     * @notice returns the Friday 8AM UTC timestamp following the next
+     * approaching Friday
      * @param timestamp is the current timestamp
-     * Reference: https://codereview.stackexchange.com/a/33532
-     * Examples:
-     * getNextFriday(week 1 thursday) -> week 2 friday
-     * getNextFriday(week 1 friday) -> week 2 friday
-     * getNextFriday(week 1 saturday) -> week 2 friday
+     * @return Friday 8am UTC timestamp
      */
     function _getNextFriday(uint256 timestamp) internal pure returns (uint256) {
-        // dayOfWeek = 0 (sunday) - 6 (saturday)
-        uint256 dayOfWeek = ((timestamp / 1 days) + 4) % 7;
-        uint256 nextFriday = timestamp + ((7 + 5 - dayOfWeek) % 7) * 1 days;
-        uint256 friday8am = nextFriday - (nextFriday % (24 hours)) + (8 hours);
-
-        // If the timestamp is on a Friday or between Monday-Thursday
-        // return Friday of the following week
-        if (
-            timestamp >= friday8am ||
-            friday8am - timestamp <= (4 days) + (8 hours)
-        ) {
-            friday8am += 7 days;
-        }
-        return friday8am;
+        return _getFriday(timestamp) + 7 days;
     }
 }
